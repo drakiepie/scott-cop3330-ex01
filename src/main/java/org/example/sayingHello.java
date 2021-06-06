e4ddd79638082434eb47a1f6c264d61533ee7452
Drake Scott <drakescott999@hotmail.com>
2021-06-05 21:50:39 -0400

    copyright

--- src/main/java/org/example/sayingHello.java
+++ src/main/java/org/example/sayingHello.java
@@ -1,12 +1,18 @@
 package org.example;
 import java.util.Scanner;
 
+/*
+ *  UCF COP3330 Summer 2021 Exercise 1 Solution
+ *  Copyright 2021 Drake Scott
+ */
+
 public class sayingHello {
     public static void main(String[] args){
         System.out.print("What is your name? ");
 
         Scanner input = new Scanner(System.in);
         String name = input.next();
-        System.out.print("Hello, " + name + ", nice to meet you!");
+        String output = "Hello, " + name + ", nice to meet you!";
+        System.out.println(output);
     }
 }
